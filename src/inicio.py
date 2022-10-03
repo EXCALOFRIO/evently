038a1600d8c7dbb13cafb7b450e57c24214aea26
Serena Blanco García <serena.blancogarcia@usp.ceu.es>
2022-10-03 20:00:51 +0200

    Interfaz Gráfica del registro

--- src/inicio.py
+++ src/inicio.py
@@ -1,37 +1,40 @@
 from tkinter import *
-    
+
+
 raiz=Tk()
 raiz.title("EVENTLY")
-raiz.config(width=500, height=500)
+raiz.geometry("300x350")
+raiz.resizable(0, 0)
     
-frame1=Frame(raiz, width=300, height=500)#,bg="lightblue")
+frame1=Frame(raiz, width=300, height=500)
 frame1.pack()
 
-inicioSesion=Label(frame1, text="Inicio de Sesión", font=("Arial", 16))
+inicioSesion=Label(frame1, text="Inicio de Sesión", font=("Arial", 20))
 inicioSesion.grid(row=0, padx=10, pady=10)
 
-usuarioLabel=Label(frame1, text="Usuario:")
+usuarioLabel=Label(frame1, text="Usuario:", font=("Arial", 12))
 usuarioLabel.grid(row=1, pady=5)
 
-contrasenaLabel=Label(frame1, text="Contraseña:")
+contrasenaLabel=Label(frame1, text="Contraseña:", font=("Arial", 12))
 contrasenaLabel.grid(row=3, pady=5)
 
 nombreUsuario=StringVar()
-usuarioEntry=Entry(frame1, textvariable=nombreUsuario)
+usuarioEntry=Entry(frame1, textvariable=nombreUsuario, font=("Arial", 12))
 usuarioEntry.grid(row=2)
 
 contrasenaUsuario=StringVar()
-contrasenaEntry=Entry(frame1, textvariable=contrasenaUsuario)
+contrasenaEntry=Entry(frame1, textvariable=contrasenaUsuario, font=("Arial", 12))
 contrasenaEntry.grid(row=4)
 contrasenaEntry.config(show="*")
 
 frame2=Frame(raiz)
 frame2.pack()
 
-botonAceptar=Button(frame2, text="Aceptar")
+botonAceptar=Button(frame2, text="Aceptar", font=("Arial", 10))
 botonAceptar.grid(column=0, row=0, padx=5, pady=10)
 
-botonRegistro=Button(frame2, text="Registro")
+
+botonRegistro=Button(frame2, text="Registro", font=("Arial", 10))
 botonRegistro.grid(column=1, row=0, padx=5, pady=10)
 
 
---
+++ src/registro.py
@@ -0,0 +1,66 @@
+from re import L
+from tkinter import *
+
+raiz=Tk()
+raiz.geometry("600x300")
+raiz.title("REGISTRO")
+
+frame=Frame(raiz, bg="lightblue")
+frame.pack()
+
+eventlyLbl=Label(frame, text="EVENTLY", font=("Arial", 14), pady=10)
+eventlyLbl.pack()
+
+frame1=Frame(raiz)
+frame1.pack()
+
+usuarioLbl=Label(frame1, text="Usuario:", font=("Arial", 12))
+usuarioLbl.grid(column=0, row=0, padx=5, pady=5)
+
+nombreLbl=Label(frame1, text="Nombre:", font=("Arial", 12))
+nombreLbl.grid(column=0, row=1, padx=5, pady=5)
+
+apellidosLbl=Label(frame1, text="Apellidos:", font=("Arial", 12))
+apellidosLbl.grid(column=0, row=2, padx=5, pady=5)
+
+contrasenaLbl=Label(frame1, text="Contraseña:", font=("Arial", 12))
+contrasenaLbl.grid(column=2, row=0, padx=5, pady=5)
+
+emailLbl=Label(frame1, text="E-mail:", font=("Arial", 12))
+emailLbl.grid(column=2, row=1, padx=5, pady=5)
+
+edadLbl=Label(frame1, text="Edad:", font=("Arial", 12))
+edadLbl.grid(column=2, row=2, padx=5, pady=5)
+
+usuario=StringVar()
+usuarioEntry=Entry(frame1, text=usuario, font=("Arial", 12))
+usuarioEntry.grid(column=1, row=0, padx=5, pady=5)
+
+nombre=StringVar()
+nombreEntry=Entry(frame1, text=nombre, font=("Arial", 12))
+nombreEntry.grid(column=1, row=1, padx=5, pady=5)
+
+apellidos=StringVar()
+apellidosEntry=Entry(frame1, text=apellidos, font=("Arial", 12))
+apellidosEntry.grid(column=1, row=2, padx=5, pady=5)
+
+contrasena=StringVar()
+contrasenaEntry=Entry(frame1, text=contrasena, font=("Arial", 12))
+contrasenaEntry.grid(column=3, row=0, padx=5, pady=5)
+contrasenaEntry.config(show="*")
+
+email=StringVar()
+emailEntry=Entry(frame1, text=email, font=("Arial", 12))
+emailEntry.grid(column=3, row=1, padx=5, pady=5)
+
+edad=IntVar()
+edadEntry=Entry(frame1, text=edad, font=("Arial", 12))
+edadEntry.grid(column=3, row=2, padx=5, pady=5)
+
+frame2=Frame(raiz, pady=20)
+frame2.pack()
+
+botonAceptar=Button(frame2, text="Aceptar", font=("Arial", 12))
+botonAceptar.pack()
+
+raiz.mainloop()
