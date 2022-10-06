4f26a0428597cab9897dcdc3ccd9a83b9da1158f
EXCALOFRIO <alejandro.ramirezlarena@usp.ceu.es>
2022-10-06 22:10:35 +0200

    Fuente mod

--- src/añadirFiesta.py
+++ src/añadirFiesta.py
@@ -5,7 +5,10 @@ from tkinter import messagebox
 from firebase_admin import db
 
 from baseDatosPrueba import insertarDiscoteca, insertarFiesta
+# pip install pyglet
+import pyglet
 
+pyglet.font.add_file('fuentes/productSans.ttf')  # ABeeZee
 
 raiz = Tk()
 raiz.geometry("600x300")
@@ -14,38 +17,38 @@ raiz.title("AÑADIENDO FIESTA")
 frame = Frame(raiz, bg="lightblue")
 frame.pack()
 
-eventlyLbl = Label(frame, text="EVENTLY", font=("Arial", 14), pady=10)
+eventlyLbl = Label(frame, text="EVENTLY", font=("ABeeZee", 14), pady=10)
 eventlyLbl.pack()
 
 frame1 = Frame(raiz)
 frame1.pack()
 
-nombreLbl = Label(frame1, text="Nombre:", font=("Arial", 12))
+nombreLbl = Label(frame1, text="Nombre:", font=("ABeeZee", 12))
 nombreLbl.grid(column=0, row=0, padx=5, pady=5)
 
-zonaLbl = Label(frame1, text="Zona:", font=("Arial", 12))
+zonaLbl = Label(frame1, text="Zona:", font=("ABeeZee", 12))
 zonaLbl.grid(column=0, row=1, padx=5, pady=5)
 
-calleLbl = Label(frame1, text="Calle:", font=("Arial", 12))
+calleLbl = Label(frame1, text="Calle:", font=("ABeeZee", 12))
 calleLbl.grid(column=0, row=2, padx=5, pady=5)
 
-numeroLbl = Label(frame1, text="Numero:", font=("Arial", 12))
+numeroLbl = Label(frame1, text="Numero:", font=("ABeeZee", 12))
 numeroLbl.grid(column=0, row=3, padx=5, pady=5)
 
 nombre = StringVar()
-nombreEntry = Entry(frame1, text=nombre, font=("Arial", 12))
+nombreEntry = Entry(frame1, text=nombre, font=("ABeeZee", 12))
 nombreEntry.grid(column=1, row=0, padx=5, pady=5)
 
 zona = StringVar()
-zonaEntry = Entry(frame1, text=zona, font=("Arial", 12))
+zonaEntry = Entry(frame1, text=zona, font=("ABeeZee", 12))
 zonaEntry.grid(column=1, row=1, padx=5, pady=5)
 
 calle = StringVar()
-calleEntry = Entry(frame1, text=calle, font=("Arial", 12))
+calleEntry = Entry(frame1, text=calle, font=("ABeeZee", 12))
 calleEntry.grid(column=1, row=2, padx=5, pady=5)
 
 numero = IntVar()
-numeroEntry = Entry(frame1, text=numero, font=("Arial", 12))
+numeroEntry = Entry(frame1, text=numero, font=("ABeeZee", 12))
 numeroEntry.grid(column=1, row=3, padx=5, pady=5)
 
 frame2 = Frame(raiz, pady=20)
@@ -53,7 +56,7 @@ frame2.pack()
 
 
 botonAceptar = Button(frame2, text="Aceptar",
-                      command=lambda: botonAceptarClick(), font=("Arial", 12))
+                      command=lambda: botonAceptarClick(), font=("ABeeZee", 12))
 botonAceptar.pack()
 
 
--- src/principal.py
+++ src/principal.py
@@ -1,5 +1,14 @@
 from tkinter import *
 
+# add a custom fonto to tkinter from a file that is in the same directory as the python file
+import tkinter as tk
+
+import pyglet
+
+pyglet.font.add_file('fuentes/productSans.ttf')#ABeeZee
+
+
+# add froma a .ttf
 inicio = Tk()
 inicio.title("INICIO")
 inicio.geometry("600x350")
@@ -9,15 +18,15 @@ frame1 = Frame(inicio, width=300, height=500)
 frame1.pack()
 # añade 3 botones de colore salmón, verde y azul para añadir discotecas añadir fiestas y valoraciones
 boton1 = Button(frame1, text="Añadir Discoteca",
-                command=lambda: botonDicotecaClick(), font=("Arial", 10))
+                command=lambda: botonDicotecaClick(), font=("ABeeZee", 10))
 boton1.grid(column=0, row=0, padx=20, pady=30)
 
 boton2 = Button(frame1, text="Añadir Fiestas",
-                command=lambda: botonFiestaClick(), font=("Arial", 10))
+                command=lambda: botonFiestaClick(), font=("ABeeZee", 10))
 boton2.grid(column=1, row=0, padx=20, pady=30)
 
 boton3 = Button(frame1, text="Añadir Valoraciones",
-                command=lambda: botonValoracionClick(), font=("Arial", 10))
+                command=lambda: botonValoracionClick(), font=("ABeeZee", 10))
 boton3.grid(column=2, row=0, padx=20, pady=30)
 
 
