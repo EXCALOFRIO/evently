ca87c5074a0d68f5ebfb406308510688e5742572
Serena Blanco García <serena.blancogarcia@usp.ceu.es>
2022-10-06 21:21:13 +0200

    TDD registro SIN TERMINAR - DA ERROR

---
+++ src/registroTDD.py
@@ -0,0 +1,25 @@
+import unittest
+import sqlite3
+from registro import insertarUsuario
+from registro import comprobarUsuario
+
+
+class TestRegistro(unittest.TestCase):
+    
+    miConexion=sqlite3.connect("BBDDpruebaReg")
+    miCursor=miConexion.cursor()
+    miCursor.execute("CREATE TABLE IF NOT EXISTS usuarios (usuario VARCHAR(100), nombre VARCHAR(100), apellido VARCHAR(100), edad INTEGER, email VARCHAR(100), contraseña VARCHAR(100))")
+    
+    
+    def test_registro(self) -> None:
+        
+        sentencia=insertarUsuario("usr", "name", "srname", 18, "name@gmail.com", "123abc")
+        resultado=comprobarUsuario("usr", "name", "srname", 18, "name@gmail.com", "123abc")
+        
+        self.assertEqual(sentencia, resultado)
+        
+    miConexion.close()
+
+
+if __name__ == '__main__':
+    unittest.main()
