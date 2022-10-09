079f186f333678c6109dd9706c58dd946865b1f1
juaki0315 <juaki0315@gmail.com>
2022-10-09 20:58:52 +0200

    Funcionalidad ver mapa en ventana + Instrucciones

--- src/mapViewer.py
+++ src/mapViewer.py
@@ -1,3 +1,6 @@
+# Importante antes de ejecutar, instalar webview con el siguiente comando:
+# pip install pywebview
+
 import webview
 
 webview.create_window('Evently - Mapa de discotecas', '../mapa.html')
