2a0fcf549488c14919775a5b342cf70137878648
Pablo Rivero <pablo.rivero1@usp.ceu.es>
2022-11-24 17:31:57 +0100

    interfaz

--- appKivy/main.py
+++ appKivy/main.py
@@ -6,15 +6,21 @@ from kivy.lang import Builder
 class Ui(ScreenManager):
     pass
 
+
 class MainApp(MDApp):
     def build(self):
         self.theme_cls.theme_style = 'Dark'
-        self.theme_cls.primary_palette = 'Indigo'
+        self.theme_cls.primary_palette = 'Blue'
         Builder.load_file('design.kv')
 
         return Ui()
 
+    def change_style(self, checked, value):
+        if value:
+            self.theme_cls.theme_style = 'Light'
+        else:
+            self.theme_cls.theme_style = 'Dark'
+
+
 if __name__ == '__main__':
     MainApp().run()
-
-
