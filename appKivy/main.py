26873a883c281151b044a0a20e21dc45ccfd4293
Pablo Rivero <pablo.rivero1@usp.ceu.es>
2022-12-08 14:12:14 +0100

    nuevos botones

--- appKivy/main.py
+++ appKivy/main.py
@@ -2,7 +2,7 @@ from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager
 from kivy.lang import Builder
 from kivy.uix.image import Image
-
+import requests
 
 class Ui(ScreenManager):
     pass
@@ -12,6 +12,8 @@ class MainApp(MDApp):
     def build(self):
         self.theme_cls.theme_style = 'Dark'
         self.theme_cls.primary_palette = 'Blue'
+        self.url = 'https://evently-646a2-default-rtdb.firebaseio.com/.json'
+        self.key = 'f56cws1P0VqbWLLiquVOReDvJONfKtq9qzeWDSIl'
         Builder.load_file('design.kv')
 
         return Ui()
